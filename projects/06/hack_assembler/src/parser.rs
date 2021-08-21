ca81ec55a3aa7557e12ba3ca31039c1f936a032e
adoringonion <adringonion.byazura@gmail.com>
2021-08-22 03:59:21 +0900

    refactoring

--- projects/06/hack_assembler/src/parser.rs
+++ projects/06/hack_assembler/src/parser.rs
@@ -1,9 +1,9 @@
+use anyhow::Result;
 use std::{
     fs::File,
-    io::{BufRead, BufReader, Lines},
+    io::{BufRead, BufReader},
     vec::IntoIter,
 };
-use anyhow::Result;
 
 pub enum CommandType {
     ACommand,
@@ -20,10 +20,14 @@ pub struct Parser {
 
 impl Parser {
     pub fn new(file: File) -> Result<Parser> {
-
-
-        let lines = BufReader::new(file).lines();
-        let tokens = pick_out_tokens(lines);
+        let tokens = BufReader::new(file)
+            .lines()
+            .filter_map(|line| line.ok())
+            .filter(|line| !line.starts_with("//"))
+            .filter(|line| !line.is_empty())
+            .map(pick_out_tokens)
+            .collect::<Vec<String>>()
+            .into_iter();
 
         let mut parser = Self {
             tokens,
@@ -31,7 +35,7 @@ impl Parser {
             next_token: None,
         };
 
-        // 初期値を設定するため、二回advanceを呼び出す
+        // call advance() twice to set the first token and the second token
         parser.advance();
         parser.advance();
 
@@ -51,7 +55,7 @@ impl Parser {
     }
 
     pub fn command_type(&self) -> CommandType {
-        let token = self.current_token.as_ref().expect("expected a command");
+        let token = self.current_token.as_ref().unwrap();
 
         if is_a_command(&token) {
             CommandType::ACommand
@@ -63,11 +67,7 @@ impl Parser {
     }
 
     pub fn symbol(&self) -> String {
-        let mut token = self
-            .current_token
-            .as_ref()
-            .expect("expected a symbol")
-            .to_string();
+        let mut token = self.current_token.as_ref().unwrap().to_string();
 
         if is_a_command(&token) {
             token.retain(|c| c != '@');
@@ -80,7 +80,7 @@ impl Parser {
     }
 
     pub fn dest(&self) -> String {
-        let current_token = self.current_token.as_ref().expect("expected a dest");
+        let current_token = self.current_token.as_ref().unwrap();
         if !current_token.contains("=") {
             return String::from("null");
         }
@@ -90,14 +90,14 @@ impl Parser {
     }
 
     pub fn comp(&self) -> String {
-        let current_token = self.current_token.as_ref().expect("expected a comp");
+        let current_token = self.current_token.as_ref().unwrap();
         if current_token.contains("=") && current_token.contains(";") {
             let equal_index = current_token.find("=").unwrap();
             let semi_index = current_token.find(";").unwrap();
-            return current_token[equal_index+1..semi_index].to_string();
+            return current_token[equal_index + 1..semi_index].to_string();
         } else if current_token.contains("=") {
             let equal_index = current_token.find("=").unwrap();
-            return current_token[equal_index+1..].to_string();
+            return current_token[equal_index + 1..].to_string();
         } else {
             let semi_index = current_token.find(";").unwrap();
             return current_token[..semi_index].to_string();
@@ -105,18 +105,14 @@ impl Parser {
     }
 
     pub fn jump(&self) -> String {
-        let current_token = self.current_token.as_ref().expect("expected a dest");
+        let current_token = self.current_token.as_ref().unwrap();
         if !current_token.contains(";") {
             return String::from("null");
         }
 
         let semi_index = current_token.find(";").unwrap();
-        return current_token[semi_index+1..].to_string();
+        return current_token[semi_index + 1..].to_string();
     }
-}
-
-fn skip_line(string: &String) -> bool {
-    string.starts_with("//") || string.is_empty()
 }
 
 fn is_a_command(line: &String) -> bool {
@@ -127,18 +123,11 @@ fn is_l_command(line: &String) -> bool {
     line.starts_with("(")
 }
 
-fn pick_out_tokens(lines: Lines<BufReader<File>>) -> IntoIter<String> {
-    let mut tokens = vec![];
-    for wrapped_line in lines {
-        let line = wrapped_line.unwrap();
-        if skip_line(&line) {
-            continue;
-        }
-
-        let split_comments: Vec<&str> = line.split("//").collect();
-        let split_whitespace: Vec<&str> = split_comments[0].split_whitespace().collect();
-        tokens.push(split_whitespace[0].to_string());
+fn pick_out_tokens(mut line: String) -> String {
+    line.retain(|c| !c.is_whitespace());
+    if let Some(index) = line.find("//") {
+        return line[..index].to_string();
+    } else {
+        return line;
     }
-
-    tokens.into_iter()
 }
