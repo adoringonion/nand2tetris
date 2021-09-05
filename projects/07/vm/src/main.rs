8e60f8b1ecd845ecdaa7ab4d2ba27640fd3f771a
adoringonion <adringonion.byazura@gmail.com>
2021-09-05 13:58:17 +0900

    wip

--- projects/07/vm/src/main.rs
+++ projects/07/vm/src/main.rs
@@ -1,6 +1,34 @@
+use anyhow::{anyhow, Context, Result};
+use std::{env, fs::File};
+
+use crate::parser::Parser;
+
 mod code_writer;
 mod parser;
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<()> {
+    let args: Vec<String> = env::args().collect();
+    let file_path = &args[1];
+    if !file_path.ends_with(".vm") {
+        return Err(anyhow!("Invalid file extension: {}", file_path));
+    }
+    let file = File::open(file_path).with_context(|| format!("not find {}", file_path))?;
+
+    let mut parser = Parser::new(file);
+
+    loop {
+        match parser.command_type() {
+            parser::CommandType::CArithmetic => todo!(),
+            parser::CommandType::CPush => todo!(),
+            _ => todo!(),
+        }
+
+        if !parser.has_more_commands() {
+            break;
+        }
+
+        parser.advance();
+    }
+
+    Ok(())
 }
--- projects/07/vm/src/parser.rs
+++ projects/07/vm/src/parser.rs
@@ -1,3 +1,10 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+    mem::replace,
+    vec::IntoIter,
+};
+
 pub enum CommandType {
     CArithmetic,
     CPush,
@@ -10,24 +17,88 @@ pub enum CommandType {
     CCall,
 }
 
-pub struct Parser {}
+pub struct Parser {
+    lines: IntoIter<Vec<String>>,
+    current_line: Option<Vec<String>>,
+    next_line: Option<Vec<String>>,
+}
 
 impl Parser {
-    fn new() -> Self {
-        Parser {}
+    pub fn new(file: File) -> Self {
+        let lines = BufReader::new(file)
+            .lines()
+            .filter_map(|line| line.ok())
+            .filter(|line| !line.starts_with("//"))
+            .filter(|line| !line.is_empty())
+            .map(remove_comments)
+            .map(|line| line.split_whitespace().map(|x| x.to_string()).collect())
+            .collect::<Vec<Vec<String>>>()
+            .into_iter();
+
+        let mut parser = Self {
+            lines,
+            current_line: None,
+            next_line: None,
+        };
+
+        // call advance() twice to set the first token and the second token
+        parser.advance();
+        parser.advance();
+
+        parser
     }
 
-    fn has_more_commands() -> bool {
-        false
+    pub fn has_more_commands(&self) -> bool {
+        if let None = self.next_line {
+            false
+        } else {
+            true
+        }
     }
 
-    fn advance() {}
-
-    fn arg1() -> String {
-        String::from("")
+    pub fn advance(&mut self) {
+        self.current_line = replace(&mut self.next_line, self.lines.next());
     }
 
-    fn arg2() -> u32 {
-        0
+    pub fn command_type(&self) -> CommandType {
+        let command = self.current_line.as_ref().unwrap();
+
+        match command[0].as_str() {
+            "add" | "sub" | "neg" | "eq" | "gt" | "lt" | "and" | "or" | "not" => {
+                CommandType::CArithmetic
+            }
+            "push" => CommandType::CPush,
+            "pop" => CommandType::CPop,
+            "label" => CommandType::CLabel,
+            "goto" => CommandType::CGoto,
+            "if-goto" => CommandType::CIf,
+            "function" => CommandType::CFunction,
+            "call" => CommandType::CCall,
+            "return" => CommandType::CReturn,
+            _ => panic!("Unknown command type: {}", command[0]),
+        }
+    }
+
+    pub fn arg1(&self) -> String {
+        let command = self.current_line.as_ref().unwrap();
+
+        match self.command_type() {
+            CommandType::CArithmetic => command[0].clone(),
+            _ => command[1].clone(),
+        }
+    }
+
+    pub fn arg2(&self) -> u32 {
+        let command = self.current_line.as_ref().unwrap();
+
+        return command[2].parse::<u32>().unwrap();
+    }
+}
+
+fn remove_comments(line: String) -> String {
+    if let Some(index) = line.find("//") {
+        return line[..index].to_string();
+    } else {
+        return line;
     }
 }
